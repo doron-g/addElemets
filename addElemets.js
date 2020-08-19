1367b9a3e8787fceb6a06f4905adc7b9161d21a6
doron <dgescheidt3@gmail.com>
2020-08-19 20:05:57 +0300

    buttons all over

--- addElemets.js
+++ addElemets.js
@@ -1,34 +1,32 @@
 counter = 1
 
 
-
-function addDiv(scale=10, title){
+function addDiv(scale=10, title, accumulative){
     const elm = document.createElement("button");
     elm.classList.add("image");
     elm.classList.add("btn");
     elm.classList.add("btn-primary");
-    elm.setAttribute('style', `--scale: ${scale};float: left`);
+    elm.setAttribute('style', `--scale: ${scale}`);
     elm.setAttribute('data-toggle','popover');
     elm.setAttribute('data-trigger', 'focus');
     elm.setAttribute('title', title);
-    elm.setAttribute('data-content','SOME DATA');
-    elm.setAttribute('data-placement','left');
+    elm.setAttribute('data-content','מתים מקורונה היום: ' + scale + " עד היום נפטרו: " + accumulative);
+    elm.setAttribute('data-placement','down');
     elm.setAttribute('type','button');
     elm.setAttribute('id',++counter);
     document.body.appendChild(elm);
 }
 
 async function getCoronaDeaths() {
-   const result = await fetch('coronaDeaths.json')
+   const result = await fetch('coronaDeaths1.json')
    const mydata = await result.json()
    for (const day of mydata) {
        if (day.deaths > 0) {
-            addDiv(day.deaths, day.date);
+            addDiv(day.deaths, day.date, day.accumulative);
 
        }
    }
    $('[data-toggle="popover"]').popover()
 }
 getCoronaDeaths()
-
 
