9e595515fdaec78f0ed8b7b8278ebd3f448faa0d
doron <dgescheidt3@gmail.com>
2020-08-19 17:56:26 +0300

    buttons

--- addElemets.js
+++ addElemets.js
@@ -1,8 +1,20 @@
+counter = 1
 
 function addDiv(scale=10){
-    const elm = document.createElement("div");
+    const elm = document.createElement("button");
     elm.classList.add("image");
-    elm.setAttribute('style', `--scale: ${scale}`);
+    elm.classList.add("btn");
+    elm.classList.add("btn-primary");
+
+
+    elm.setAttribute('style', `--scale: ${scale};float: left`);
+    elm.setAttribute('data-toggle','popover');
+    elm.setAttribute('data-trigger', 'focus');
+    elm.setAttribute('title','TITLE');
+    elm.setAttribute('data-content','SOME DATA');
+    elm.setAttribute('data-placement','left');
+    elm.setAttribute('type','button');
+    elm.setAttribute('id',++counter);
     document.body.appendChild(elm);
 }
 
@@ -11,9 +23,24 @@ async function getCoronaDeaths() {
    const mydata = await result.json()
    for (const day of mydata) {
        if (day.deaths > 0) {
-            addDiv(day.deaths)
+            addDiv(day.deaths);
+//            do something with day.date to put it in popover
        }
    }
 }
 getCoronaDeaths()
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+});
+
+$("popover-dismiss").popover(
+{
+    trigger: "focus"
+});
+
+$('#12').popover(
+{
+    trigger: "hover"
+});
 
